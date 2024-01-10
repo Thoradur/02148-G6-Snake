b00608b843ad15fd57fc98ec7b0c0cd325808be9
Muneeb2002 <mmuneebfar@gmail.com>
2024-01-10 13:44:53 +0100

    Create Fruit.java

---
+++ src/main/java/snake/state/Fruit.java
@@ -0,0 +1,54 @@
+package snake.state;
+
+import java.util.Random;
+
+import snake.common.Point;
+import snake.protocol.state.Fragment;
+
+public class Fruit implements GameObject {
+    
+    Point position;
+    
+    public Fruit(Point position) {
+        this.position = position;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    @Override
+    public void build(Board board) {
+
+        
+    }
+
+    public void positionFruit(Board board, int seed) {
+        Random random = new Random(seed);
+        while(true){
+            int x = random.nextInt(board.getWidth());
+            int y = random.nextInt(board.getHeight());
+            Point position = new Point(x, y);
+            if(board.getCell(position).getStack().isEmpty()){
+                setPosition(position);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public int getStep() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStep'");
+    }
+
+    @Override
+    public Fragment step() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'step'");
+    }
+}
